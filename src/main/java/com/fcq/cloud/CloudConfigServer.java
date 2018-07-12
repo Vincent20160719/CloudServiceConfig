adb358c046e26620030435b5aeb270a62dabc50a
fuchongqing <fuchongqing@learnta.com>
2018-07-12 18:03:17 +0800

    commit

--- src/main/java/com/fcq/cloud/CloudConfigServer.java
+++ src/main/java/com/fcq/cloud/CloudConfigServer.java
@@ -2,10 +2,13 @@ package com.fcq.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudConfigServer {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigServer.class, args);
+		
 	}
 }
