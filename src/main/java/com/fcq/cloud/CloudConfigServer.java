55de4f3c919f6b1e4ae8a3f34314587f3a5dc116
fuchongqing <fuchongqing@learnta.com>
2018-07-12 15:24:12 +0800

    create server config server

---
+++ src/main/java/com/fcq/cloud/CloudConfigServer.java
@@ -0,0 +1,11 @@
+package com.fcq.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServer {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServer.class, args);
+	}
+}
